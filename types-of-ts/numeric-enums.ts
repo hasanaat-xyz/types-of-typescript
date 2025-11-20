58c29b7da6b1c3575829d66fd4e764a515a37758
hasanaat <arsheema619@gmail.com>
2025-11-20 12:34:12 +0530

    numeric enums

--- types-of-ts/numeric-enums.ts
+++ types-of-ts/numeric-enums.ts
@@ -2,8 +2,7 @@ enum Direction {
   Up,       // 0
   Down,     // 1
   Left,     // 2
-  Right   
-    // 3
+  Right     // 3
 }
 console.log(Direction.Up);        // 0
 console.log(Direction[0]);        // "Up"  ← reverse mapping
